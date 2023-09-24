2d5fed1a59ad96a6ed500d801cee8c3a8685e379
Abhisshek Gupta <abhishekgupta.241081@gmail.com>
2023-09-24 21:18:19 +0530

    back to top added

--- main.js
+++ main.js
@@ -65,3 +65,19 @@ window.addEventListener('scroll', () => {
 })
 
 
+$('footer').append('<div id="back-to-top"><a href="#"><i class="fa fa-angle-up"></i></a></div>');
+$("#back-to-top").hide();
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        $("#back-to-top").fadeIn(500);
+    } else {
+        $("#back-to-top").fadeOut(500);
+    }
+});
+//back to top
+$("#back-to-top").click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
